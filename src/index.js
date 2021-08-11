51629e562ef4b320e2abde7c5a6a869d201c7283
Ramazan Mustafaev <chiton0@gmail.com>
2021-08-11 15:12:18 +0300

    feat: add realisaton of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let reversedNumber = 0;
+    while (n > 0) {
+        reversedNumber = reversedNumber * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return reversedNumber;
 }
